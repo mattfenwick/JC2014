61cc4e3e2f3414a85c517e115073322644083d05
Matt Fenwick <mfenwick100@gmail.com>
2014-09-22 15:20:27 -0400

    new function

--- getdata.py
+++ getdata.py
@@ -27,6 +27,16 @@ for r in sav.loops[1].rows:
 #  1. find the overall amount of CA overlap
 #  2. find residues with similar CA/C shifts
 
+def print_residue(ix, r):
+  order = ['H', 'N', 'C', 'CA']
+  my_string = ['id: %s%i' % (r['aatype'], ix)]
+  for o in order:
+    if o in r:
+      my_string.append('%s: %s' % (o, r[o]))
+    else:
+      my_string.append('%s: -----' % o)
+  print '  '.join(my_string), '    '
+
 # 2
 def get_overlap(tol_c=0.1, tol_ca=0.1):
   for (i1,r1) in residues.items():
@@ -35,8 +45,8 @@ def get_overlap(tol_c=0.1, tol_ca=0.1):
         continue
       if 'C' in r1 and 'C' in r2 and (abs(r1['C'] - r2['C']) < tol_c):
         if 'CA' in r1 and 'CA' in r2 and (abs(r1['CA'] - r2['CA']) < tol_ca):
-          print i1, r1
-          print i2, r2
+          print_residue(i1, r1)
+          print_residue(i2, r2)
           print
 # better idea: build them into groups by shifts, that
 #  way, more than 2 can be in the same group
